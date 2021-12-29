2443ac16c1037bca61c50a54aaa6405205481310
divya <divya.bharathi@i-exceed.com>
2021-12-29 10:24:38 +0530

    updating ios TestRunner

--- src/test/java/com/iexceed/uiframework/runners/TestRunnerIOS.java
+++ src/test/java/com/iexceed/uiframework/runners/TestRunnerIOS.java
@@ -5,6 +5,10 @@ import com.iexceed.uiframework.utilites.WaitUtility;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.TestNGCucumberRunner;
+import lombok.SneakyThrows;
+import org.testng.annotations.AfterClass;
+
+import static com.iexceed.uiframework.domainobjects.ConnectionObjects.driver;
 
 //@RunWith(Cucumber.class)
 
@@ -27,6 +31,13 @@ public class TestRunnerIOS extends AbstractTestNGCucumberTests {
         private TestNGCucumberRunner testNGCucumberRunner;
         private final WaitUtility waitUtility= new WaitUtility();
 
-
+        @SneakyThrows
+        @AfterClass(alwaysRun = true)
+        public void tearDownClass() {
+                driver.quit();
+                System.out.println("driver is closed now");
+                waitUtility.waitForSeconds(77);
+//                testNGCucumberRunner.finish();
+        }
 
 }
