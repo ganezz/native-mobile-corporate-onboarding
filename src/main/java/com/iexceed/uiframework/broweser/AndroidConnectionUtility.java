db9969daf715002153b30419931cfaa5d745bb1a
divya <divya.bharathi@i-exceed.com>
2022-01-04 14:03:25 +0530

    android device finding utility class updation

--- src/main/java/com/iexceed/uiframework/broweser/AndroidConnectionUtility.java
+++ src/main/java/com/iexceed/uiframework/broweser/AndroidConnectionUtility.java
@@ -18,7 +18,7 @@ public class AndroidConnectionUtility {
         List<String> list = new ArrayList<>();
         list.add("LENOVO_M10Plus_Android_10.0.0_2ba36");
         list.add("SAMSUNG_GalaxyTabS4_Android_10.0.0_2133b");
-        list.add("SAMSUNG_GalaxyTabS6_Android_9.0.0_16c48");
+        list.add("SAMSUNG_GalaxyTabS6_Android_11.0.0_383e3");
         for (String appName : list) {
 
                 if (driver == null) {
