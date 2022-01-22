b638b06a2af1c7a503a18efa5406f06696965ac8
divya <divya.bharathi@i-exceed.com>
2022-01-22 15:14:57 +0530

    udating utility class

--- src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
+++ src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
@@ -10,9 +10,9 @@ import io.cucumber.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
 
-        features = "src/test/resources/Features/LegalEntBusinessLocation.feature",
+        features = "src/test/resources/Features",
         glue = {"com.iexceed.uiframework.stepdefinitions"},
-        tags= "@1",
+        tags= "@regressionHomePage",
         plugin= {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
