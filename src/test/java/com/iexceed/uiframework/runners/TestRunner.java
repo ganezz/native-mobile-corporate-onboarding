0e06dd2f71a7b8b479a4a1221991095e3b5d0c57
divya <divya.bharathi@i-exceed.com>
2022-01-23 10:56:26 +0530

    Resolving cucumber conflicts'

--- src/test/java/com/iexceed/uiframework/runners/TestRunner.java
+++ src/test/java/com/iexceed/uiframework/runners/TestRunner.java
@@ -7,20 +7,23 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.TestNGCucumberRunner;
 import lombok.SneakyThrows;
 import org.testng.annotations.AfterClass;
+
 import static com.iexceed.uiframework.domainobjects.ConnectionObjects.driver;
 //@RunWith(Cucumber.class)
 
 @CucumberOptions(
-
-        features = "src/test/resources/Features",
-        glue = {"com.iexceed.uiframework.stepdefinitions"},
-        tags= "@regressionHomePage",
-        plugin= {"pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
-                "rerun:target/failedrerun.txt"
-
-        }
+//
+//        features = "src/test/resources/Features",
+//        glue = {"com.iexceed.uiframework.stepdefinitions"},
+//        tags= "@sanity_apps",
+//        plugin= {"pretty",
+//                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//                "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
+//                "rerun:target/failedrerun.txt"
+//
+//
+//
+//        }
 
 
 )
@@ -29,7 +32,23 @@ public class TestRunner extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
     private final WaitUtility waitUtility= new WaitUtility();
 
-
+//    @BeforeClass(alwaysRun = true)
+//    public void setUpClass() {
+//        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//    }
+//
+//    @Test(dataProvider = "features")
+//    public void feature(PickleWrapper eventwrapper, FeatureWrapper cucumberFeature) throws Throwable {
+//        //testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//        testNGCucumberRunner.runScenario(eventwrapper.getPickle());
+//    }
+//
+//    @DataProvider//(parallel=true)
+//    public Object[][] features() {
+//        // return testNGCucumberRunner.provideFeatures();
+//        return testNGCucumberRunner.provideScenarios();
+//    }
+//
 
     @SneakyThrows
     @AfterClass(alwaysRun = true)
@@ -42,39 +61,11 @@ public class TestRunner extends AbstractTestNGCucumberTests {
     }
 
 
+//    @BeforeTest
+//    @Parameters("deviceName")
+//    public void parameterTest(String deviceName) {
+//        System.out.println("Parameterized value deviceName : " + deviceName);
+//            DeviceName=deviceName;
+//    }
 
-//        @BeforeClass(alwaysRun = true)
-//        public void setUpClass() {
-//                testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-//        }
-//
-//        @Test(dataProvider = "features")
-//        public void feature(PickleWrapper eventwrapper, FeatureWrapper cucumberFeature) throws Throwable {
-//                //testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-//                testNGCucumberRunner.runScenario(eventwrapper.getPickle());
-//        }
-//
-//        @DataProvider//(parallel=true)
-//        public Object[][] features() {
-//                // return testNGCucumberRunner.provideFeatures();
-//                return testNGCucumberRunner.provideScenarios();
-//        }
-//
-//        @SneakyThrows
-//        @AfterClass(alwaysRun = true)
-//        public void tearDownClass() {
-//
-//                driver.quit();
-//                System.out.println("driver is closed now");
-//                waitUtility.waitForSeconds(64);
-////                testNGCucumberRunner.finish();
-//        }
-
-//        @BeforeTest
-//        @Parameters("deviceName")
-//        public void parameterTest(String deviceName) {
-//                System.out.println("Parameterized value deviceName : " + deviceName);
-//                DeviceName=deviceName;
-//        }
-
-}
+    }
---
+++ src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
@@ -0,0 +1,65 @@
+package com.iexceed.uiframework.runners;
+
+
+import com.iexceed.uiframework.utilites.WaitUtility;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
+
+//@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+        features = "src/test/resources/Features",
+        glue = {"com.iexceed.uiframework.stepdefinitions"},
+        tags= "@regressionHomePage",
+        plugin= {"pretty",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
+                "rerun:target/failedrerun.txt"
+
+        }
+
+
+)
+
+public class TestRunnerAndroid extends AbstractTestNGCucumberTests {
+        private TestNGCucumberRunner testNGCucumberRunner;
+        private final WaitUtility waitUtility= new WaitUtility();
+
+
+//        @BeforeClass(alwaysRun = true)
+//        public void setUpClass() {
+//                testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//        }
+//
+//        @Test(dataProvider = "features")
+//        public void feature(PickleWrapper eventwrapper, FeatureWrapper cucumberFeature) throws Throwable {
+//                //testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//                testNGCucumberRunner.runScenario(eventwrapper.getPickle());
+//        }
+//
+//        @DataProvider//(parallel=true)
+//        public Object[][] features() {
+//                // return testNGCucumberRunner.provideFeatures();
+//                return testNGCucumberRunner.provideScenarios();
+//        }
+//
+//        @SneakyThrows
+//        @AfterClass(alwaysRun = true)
+//        public void tearDownClass() {
+//
+//                driver.quit();
+//                System.out.println("driver is closed now");
+//                waitUtility.waitForSeconds(64);
+////                testNGCucumberRunner.finish();
+//        }
+
+//        @BeforeTest
+//        @Parameters("deviceName")
+//        public void parameterTest(String deviceName) {
+//                System.out.println("Parameterized value deviceName : " + deviceName);
+//                DeviceName=deviceName;
+//        }
+
+}
