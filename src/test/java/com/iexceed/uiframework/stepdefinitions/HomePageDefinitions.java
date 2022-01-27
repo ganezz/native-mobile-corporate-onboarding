3514c6677cf3fff689e4ede1bec909c6ad72042a
divya <divya.bharathi@i-exceed.com>
2022-01-27 11:32:20 +0530

    updatng apk link

--- src/test/java/com/iexceed/uiframework/stepdefinitions/HomePageDefinitions.java
+++ src/test/java/com/iexceed/uiframework/stepdefinitions/HomePageDefinitions.java
@@ -38,7 +38,7 @@ public class HomePageDefinitions extends TestBase {
             testdata.setTestDataInMap(deviceConfig);
             testdata.setTestDataInMap(datalist);
 //            androidAppURL=launchApp();
-            androidAppURL="https://artifactory.appzillon.com/artifactory/android-apk/ao/manual/qaRelease-1.0.15-21-01-2022-14:05.apk";
+            androidAppURL="https://artifactory.appzillon.com/artifactory/android-apk/ao/manual/qaRelease-1.0.16-27-01-2022-10:18.apk";
             ExcelHandler.UpdateTestDataToExcel(props.getProperty("appSheetPath"), props.getProperty("deviceSheetName"),"oldAppURL",androidAppURL,props.getProperty(testcase));
             testdata.setTestDataInMap(deviceConfig);
           log.debug("updated url:"+deviceConfig.get("oldAppURL"));
