febfc267a91c168ca97049a178f6485f4f97f29d
divya <divya.bharathi@i-exceed.com>
2022-02-16 21:09:21 +0530

    made changes in test runner

--- src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
+++ src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
@@ -10,9 +10,9 @@ import io.cucumber.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
 
-        features = "src/test/resources/Features/IOSEntityUserDetails.feature",
+        features = "src/test/resources/Features",
         glue = {"com.iexceed.uiframework.stepdefinitions"},
-        tags= "@f",
+        tags= "@regressionIOSUserDetailsScreen",
         plugin= {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
