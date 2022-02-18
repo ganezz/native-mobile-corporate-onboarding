f011d9f1fde4872cd5ff4c0adf6a9590ade93bf9
divya <divya.bharathi@i-exceed.com>
2022-02-18 15:09:30 +0530

    made changes in test Runner

--- src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
+++ src/test/java/com/iexceed/uiframework/runners/TestRunnerAndroid.java
@@ -10,9 +10,9 @@ import io.cucumber.testng.TestNGCucumberRunner;
 
 @CucumberOptions(
 
-        features = "src/test/resources/Features/LegalEntCommercialLicense.feature",
+        features = "src/test/resources/Features",
         glue = {"com.iexceed.uiframework.stepdefinitions"},
-        tags= "@continue",
+        tags= "@regressionCommerciallicense",
         plugin= {"pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "json:target/jsonReports/cucumber-reports.json","html:target/cucumber-ui-reports.html",
